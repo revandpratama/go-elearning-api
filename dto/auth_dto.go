9d068d3886ad2180bbaaf5ecad1b335d8fbe372b
revandpratama <revandaustin@gmail.com>
2024-05-26 06:31:45 +0700

    refactor: moving register dto from user to auth and changing a few struct attribute

--- dto/auth_dto.go
+++ dto/auth_dto.go
@@ -9,8 +9,12 @@ type LoginResponse struct {
 	Token string `json:"token"`
 }
 
-
-
-
-
-
+type RegisterRequest struct {
+	Role            string `json:"role" binding:"required"`
+	Name            string `json:"name" binding:"required" validate:"min=3,max=50"`
+	Username        string `json:"username" binding:"required" validate:"min=3,max=50,alphanum"`
+	Email           string `json:"email" binding:"required" validate:"email"`
+	Phone           string `json:"phone" binding:"required" validate:"min=10,max=15,numeric"`
+	Password        string `json:"password" binding:"required" validate:"min=8,max=50"`
+	PasswordConfirm string `json:"password_confirm" binding:"required" validate:"min=8,max=50"`
+}
--- dto/user_dto.go
+++ dto/user_dto.go
@@ -10,11 +10,3 @@ type UserResponse struct {
 }
 
 
-type RegisterUserRequest struct {
-	Name            string `json:"name" binding:"required" validate:"min=3,max=50"`
-	Username        string `json:"username" binding:"required" validate:"min=3,max=50,alphanum"`
-	Email           string `json:"email" binding:"required" validate:"email"`
-	Phone           string `json:"phone" binding:"required" validate:"min=10,max=15,numeric"`
-	Password        string `json:"password" binding:"required" validate:"min=8,max=50"`
-	PasswordConfirm string `json:"password_confirm" binding:"required" validate:"min=8,max=50"`
-}
