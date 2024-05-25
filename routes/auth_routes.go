701c6cb4ffb405dca56c5a8b495a9c5cf502ab82
revandpratama <revandaustin@gmail.com>
2024-05-26 00:18:36 +0700

    feat: adding login functionality

---
+++ routes/auth_routes.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/revandpratama/go-elearning-api/api"
+	"github.com/revandpratama/go-elearning-api/config"
+	"github.com/revandpratama/go-elearning-api/repository"
+	"github.com/revandpratama/go-elearning-api/service"
+)
+
+func AuthRoutes(r *gin.RouterGroup) {
+	repo := repository.NewUserRepository(config.DB)
+	service := service.NewAuthService(repo)
+	handler := api.NewAuthAPI(service)
+
+
+	r.POST("/login", handler.Login)
+}
--- service/auth_service.go
+++ service/auth_service.go
@@ -1,6 +1,52 @@
 package service
 
+import (
+	"strings"
+
+	"github.com/revandpratama/go-elearning-api/dto"
+	"github.com/revandpratama/go-elearning-api/helper"
+	"github.com/revandpratama/go-elearning-api/model"
+	"github.com/revandpratama/go-elearning-api/repository"
+)
 
 type authService struct {
-	repo repository.userRepository
+	repository repository.UserRepository
+}
+
+type AuthService interface {
+	Login(LoginRequest *dto.LoginRequest) (*string, error)
+}
+
+func NewAuthService(r repository.UserRepository) *authService {
+	return &authService{
+		repository: r,
+	}
+}
+
+func (s *authService) Login(LoginRequest *dto.LoginRequest) (*string, error) {
+	var userDB *model.User
+	var err error
+	var token *string
+	if strings.Contains(LoginRequest.UsernameOrEmail, "@") {
+		userDB, err = s.repository.GetUserByEmail(LoginRequest.UsernameOrEmail)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		userDB, err = s.repository.GetUserByUsername(LoginRequest.UsernameOrEmail)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if err := helper.VerifyPassword(userDB.Password, LoginRequest.Password); err != nil {
+		return nil, err
+	}
+
+	token, err = helper.GenerateToken(userDB)
+	if err != nil {
+		return nil, err
+	}
+
+	return token, nil
 }
