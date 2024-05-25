c49c5a28bd2908af33e3bdd43c1a274d2a902153
revandpratama <revandaustin@gmail.com>
2024-05-26 00:20:35 +0700

    feat: add helper for token generation and validation

--- helper/token.go
+++ helper/token.go
@@ -1,11 +1,36 @@
 package helper
 
-// import "github.com/revandpratama/go-elearning-api/model"
+import (
+	"time"
 
-// func GenerateToken(user model.User) (string, error) {
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/revandpratama/go-elearning-api/model"
+)
 
-// }
+type JWTCustomClaims struct {
+	ID int
+	jwt.RegisteredClaims
+}
 
-// func ValidateToken(tokenString string) () {
+var privateKey = []byte("this is between us")
 
-// }
+func GenerateToken(user *model.User) (*string, error) {
+	claims := &JWTCustomClaims{
+		user.ID,
+		jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 24)),
+			IssuedAt: jwt.NewNumericDate(time.Now()),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	ss, err := token.SignedString(privateKey)
+
+	return &ss, err
+}
+
+func ValidateToken(tokenString string) {
+
+}
