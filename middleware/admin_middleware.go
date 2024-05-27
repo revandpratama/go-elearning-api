da34d5b610998bd064c5ded4d91d7dc4fd630a33
revandpratama <revandaustin@gmail.com>
2024-05-27 13:00:56 +0700

    feat:  add middleware for admin

--- middleware/admin_middleware.go
+++ middleware/admin_middleware.go
@@ -3,13 +3,27 @@ package middleware
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/revandpratama/go-elearning-api/errorhandler"
+	"github.com/revandpratama/go-elearning-api/helper"
 )
 
 func IsAdmin() gin.HandlerFunc {
 	return func(g *gin.Context) {
-		role := g.GetString("userRole")
+		tokenString, err := g.Cookie("auth_token")
 
-		if role != "admin" {
+		if err != nil || tokenString == "" {
+			errorhandler.HandleError(g, &errorhandler.UnauthorizedError{Message: "Unauthorized"})
+			g.Abort()
+			return
+		}
+
+		_, role, err := helper.ValidateToken(tokenString)
+		if err != nil {
+			errorhandler.HandleError(g, &errorhandler.UnauthorizedError{Message: err.Error()})
+			g.Abort()
+			return
+		}
+
+		if *role != "admin" {
 			errorhandler.HandleError(g, &errorhandler.UnauthorizedError{Message: "Unauthorized | only admin"})
 			g.Abort()
 			return
