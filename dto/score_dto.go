244dc5b24b75efa73bbd7bbcd1f653b0b088d821
revandpratama <revandaustin@gmail.com>
2024-05-30 23:01:55 +0700

    refactor: score dto struct update > add UserID on score request and response

--- dto/score_dto.go
+++ dto/score_dto.go
@@ -1,12 +1,14 @@
 package dto
 
 type ScoreRequest struct {
-	KrsID int `json:"krs_id" binding:"required" validate:"numeric"`
-	Score int `json:"score"`
+	UserID int `json:"user_id" binding:"required" validate:"numeric"`
+	KrsID  int `json:"krs_id" binding:"required" validate:"numeric"`
+	Score  int `json:"score"`
 }
 
 type ScoreResponse struct {
-	ID    int `json:"id"`
-	KrsID int `json:"krs_id"`
-	Score int `json:"score"`
+	ID     int `json:"id"`
+	UserID int `json:"user_id"`
+	KrsID  int `json:"krs_id"`
+	Score  int `json:"score"`
 }
