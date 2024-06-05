51e05991b1a7c943b9269fda75b7de753486718e
revandpratama <revandaustin@gmail.com>
2024-06-05 19:33:46 +0700

    fix: fix auth routes

--- routes/auth_routes.go
+++ routes/auth_routes.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/revandpratama/go-elearning-api/api"
 	"github.com/revandpratama/go-elearning-api/config"
+	"github.com/revandpratama/go-elearning-api/middleware"
 	"github.com/revandpratama/go-elearning-api/repository"
 	"github.com/revandpratama/go-elearning-api/service"
 )
@@ -13,7 +14,12 @@ func AuthRoutes(r *gin.RouterGroup) {
 	service := service.NewAuthService(repo)
 	handler := api.NewAuthAPI(service)
 
+	guestRoutes := r.Group("/")
+	guestRoutes.Use(middleware.Guest())
+	guestRoutes.POST("/login", handler.Login)
+	guestRoutes.POST("/register", handler.Register)
 
-	r.POST("/login", handler.Login)
-	r.POST("/register", handler.Register)
+	authRoutes := r.Group("/")
+	authRoutes.Use(middleware.Auth())
+	authRoutes.POST("/logout", handler.Logout)
 }
