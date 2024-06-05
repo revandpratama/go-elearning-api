3d1a135ef95924e5279233b92557fdc6fe069d92
revandpratama <revandaustin@gmail.com>
2024-06-05 19:09:59 +0700

    fix: relocate user authorization function

--- service/user_service.go
+++ service/user_service.go
@@ -1,10 +1,6 @@
 package service
 
 import (
-	"fmt"
-
-	"github.com/gin-gonic/gin"
-	"github.com/revandpratama/go-elearning-api/errorhandler"
 	"github.com/revandpratama/go-elearning-api/model"
 	"github.com/revandpratama/go-elearning-api/repository"
 )
@@ -18,7 +14,6 @@ type userService struct {
 type UserService interface {
 	GetKRSByUserID(userid int) (*[]model.KRS, error)
 	GetScoreByUserID(userid int) (*[]model.Score, error)
-	UserAuthorized(g *gin.Context, id int) error
 }
 
 func NewUserService(userrepo repository.UserRepository, krsrepo repository.KRSRepository, scorerepo repository.ScoreRepository) *userService {
@@ -39,16 +34,4 @@ func (s *userService) GetKRSByUserID(userid int) (*[]model.KRS, error) {
 
 func (s *userService) GetScoreByUserID(userid int) (*[]model.Score, error) {
 	return s.scorerepo.GetByUserID(userid)
-}
-
-func (s *userService) UserAuthorized(g *gin.Context, id int) error {
-	authID := g.MustGet("userID").(*int)
-	authRole := g.MustGet("userRole").(*string)
-	fmt.Println(id)
-	fmt.Println(authID)
-	fmt.Println(authRole)
-	if id == *authID || *authRole == "admin" {
-		return nil
-	}
-	return &errorhandler.UnauthorizedError{Message: "Unauthorized Request"}
 }
