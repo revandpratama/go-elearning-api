a17fff73e9c5a9ccaa7609a259865d703736b5a6
revandpratama <revandaustin@gmail.com>
2024-06-16 11:17:14 +0700

    refactor: change GetAll func to retrieve data behaviour base off the pagination

--- repository/score_repository.go
+++ repository/score_repository.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"errors"
 
+	"github.com/revandpratama/go-elearning-api/dto"
 	"github.com/revandpratama/go-elearning-api/model"
 	"gorm.io/gorm"
 )
@@ -15,7 +16,7 @@ type ScoreRepository interface {
 	Create(krs *model.Score) error
 	Update(id int, score *model.Score) error
 	Delete(id int) error
-	GetAll() (*[]model.Score, error)
+	GetAll(paginate *dto.Paginate) (*[]model.Score, error)
 	GetById(id int) (*model.Score, error)
 	GetByUserID(userid int) (*[]model.Score, error)
 }
@@ -41,10 +42,15 @@ func (r *scoreRepository) Delete(id int) error {
 	return err
 }
 
-func (r *scoreRepository) GetAll() (*[]model.Score, error) {
-	var score []model.Score
-	err := r.db.Find(&score).Error
-	return &score, err
+func (r *scoreRepository) GetAll(paginate *dto.Paginate) (*[]model.Score, error) {
+	var scores []model.Score
+	err := r.db.Offset((paginate.CurrentPage - 1) * 10).Limit(paginate.DataPerPage).Find(&scores).Error
+	if len(scores) < 1 {
+		return nil, errors.New("record not found")
+	}
+	// err := r.db.Find(&score).Error
+
+	return &scores, err
 }
 
 func (r *scoreRepository) GetById(id int) (*model.Score, error) {
